22c0c4a188ea6d760a6a4dd51bfdd05105b9463a
Andreas Remdt <andreas.remdt@gmail.com>
2017-10-27 10:28:03 -0400

    Finished responsive design on homepage Improved and added new content Fixed a few issues with the CSS and HTML code Added responsive navigation Optimazations

--- dest/js/main.js
+++ dest/js/main.js
@@ -9,11 +9,28 @@
 
     [].forEach.call(tabButtons, (button) => {
         button.addEventListener('click', (event) => {
+            [].forEach.call(tabButtons, (btn) => {
+                btn.classList.remove('-active');
+            });
+
+            button.classList.add('-active');
+
             [].forEach.call(tabContents, (tabContent) => {
                 tabContent.dataset.tab === button.dataset.target ? tabContent.removeAttribute('hidden') : tabContent.setAttribute('hidden', true);
             });
         }, false);
     });
 
+
+    // Navigation Toggle
+    // -------------------------------------
+
+    const menuToggle = document.querySelector('button[data-action="menu"]'),
+          navigation = document.querySelector('nav.main-navigation');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('-active');
+        navigation.classList.toggle('-opened');
+    });
 
 })();
