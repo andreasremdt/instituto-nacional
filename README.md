74e0c9f49acc6f9634408318c34b4a060023740b
Andreas Remdt <andreas.remdt@gmail.com>
2018-02-02 15:12:59 -0400

    Added site to Jekyll

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instituto Nacional
+
+website
---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    // Tabs
+    // -------------------------------------
+
+    const tabButtons = document.querySelectorAll('button[data-action="tab"]'),
+          tabContents = document.querySelectorAll('[data-tab]');
+
+    [].forEach.call(tabButtons, (button) => {
+        button.addEventListener('click', (event) => {
+            [].forEach.call(tabButtons, (btn) => {
+                btn.classList.remove('-active');
+            });
+
+            button.classList.add('-active');
+
+            [].forEach.call(tabContents, (tabContent) => {
+                tabContent.dataset.tab === button.dataset.target ? tabContent.removeAttribute('hidden') : tabContent.setAttribute('hidden', true);
+            });
+        }, false);
+    });
+
+
+    // Navigation Toggle
+    // -------------------------------------
+
+    const menuToggle = document.querySelector('button[data-action="menu"]'),
+          navigation = document.querySelector('nav.main-navigation');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('-active');
+        navigation.classList.toggle('-opened');
+    });
+
+})();
